f537e6d37404ec55742e671eb4edc5ae0e428465
Daniel Jurca <jurca@develogi.cz>
2024-10-08 23:40:35 +0200

    Add GTM script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="cs">
+      <head>
+        <script src="/gtmScript.js" async />
+      </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MG38XPHV"
+            height="0"
+            width="0"
+            className="hidden"
+          ></iframe>
+        </noscript>
+
         <JotaiProvider>
           <GoogleMapsProvider>
             <Toaster richColors closeButton position="top-center" />
